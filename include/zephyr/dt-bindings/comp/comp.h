08ea2aca800ccb7b6967171766edd0297651e4d5
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-01-25 00:37:09 +0100

    stm32: drivers: add comparator support
    Add comparator driver for ST's stm32 series.
    
    Signed-off-by: Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>

---
+++ include/zephyr/dt-bindings/comp/comp.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_COMP_COMP_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_COMP_COMP_H_
+
+#include <zephyr/dt-bindings/dt-util.h>
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_COMP_COMP_H_ */
