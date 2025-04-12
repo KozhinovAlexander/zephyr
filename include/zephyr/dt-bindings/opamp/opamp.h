1053abd0827bfc22603ec2d5407cf8fecd6a2d30
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-04-12 23:55:12 +0200

    drivers/opamp

---
+++ include/zephyr/dt-bindings/opamp/opamp.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_OPAMP_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_OPAMP_H_
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_OPAMP_H_ */
