a618da6f196f5a369f06552fca55995e373d9d16
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-05-14 00:05:05 +0200

    boards

--- boards/st/b_g431b_esc1/board.cmake
+++ boards/st/b_g431b_esc1/board.cmake
@@ -1,6 +1,12 @@
 # Copyright (c) 2024 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd.cfg")
+board_runner_args(pyocd "--target=stm32g431rbtx")
+board_runner_args(jlink "--device=STM32G431RB" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
