441fbdaaf54443f584df3ea98b95e229167d45b9
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-05-31 22:39:33 +0200

    include/

--- include/zephyr/dt-bindings/comparator/stm32g4_comp.h
+++ include/zephyr/dt-bindings/comparator/stm32g4_comp.h
@@ -8,20 +8,4 @@
 
 #include <zephyr/dt-bindings/comparator/stm32_comp.h>
 
-/* STM32 ADC resolution register for H7 and similar */
-#define STM32_ADC_RES_REG	0x0C
-#define STM32_ADC_RES_SHIFT	2
-#define STM32_ADC_RES_MASK	BIT_MASK(3)
-
-/*
- * For STM32H72x & H73x, ADC3 is different and has a 12 to 6-bit resolution.
- * The offset and the width of the resolution field need to be redefined.
- */
-#define STM32H72X_ADC3_RES_SHIFT	3
-#define STM32H72X_ADC3_RES_MASK		0x03
-
-#define STM32H72X_ADC3_RES(resolution, reg_val)	\
-	STM32_ADC(resolution, reg_val, STM32H72X_ADC3_RES_MASK, \
-		  STM32H72X_ADC3_RES_SHIFT, STM32_ADC_RES_REG)
-
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_COMPARATOR_STM32G4_COMP_H_ */
