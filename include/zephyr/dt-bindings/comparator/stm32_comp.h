7e1370b0ccafe818e49489a224034230377dddd7
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-06-01 11:09:29 +0200

    include/

---
+++ include/zephyr/dt-bindings/comparator/stm32_comp.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_COMPARATOR_STM32_COMP_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_COMPARATOR_STM32_COMP_H_
+
+#include <zephyr/dt-bindings/comparator/comp.h>
+
+#include <stm32_ll_comp.h>
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_COMPARATOR_STM32_COMP_H_ */
